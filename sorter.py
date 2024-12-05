c5e798e69bebd696099cf50ac3d30e40fca3ca0a
ThatgirlAm <atm_diop@icloud.com>
2024-12-05 18:21:53 +0000

    dev: initiating merge sorting

--- sorter.py
+++ sorter.py
@@ -1,6 +1,8 @@
 # this is the sorter file
 
 import utilities
+
+# radix sorting method
 def radix_number(array):
     max_digits = utilities.max_digit(array)
     dictionary = utilities.dictionary(array)
@@ -58,6 +60,8 @@ def radix_string(array):
         output_array = temp_output
     return output_array, dictionary
 
+
+# merge sorting method
 
 
 # Examples
