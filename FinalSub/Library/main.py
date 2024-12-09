a8fe8739ee6091ea3e3f36bdb75b51a3f16e2f12
gillmansher <gxmsrt@gmail.com>
2024-12-09 03:44:42 +0000

    F.1

--- FinalSub/Library/main.py
+++ FinalSub/Library/main.py
@@ -33,14 +33,16 @@ def enterData():
         Return arrays of either string or number data.
     """
 
-    user_data_input = input("Enter elements separated by spaces (mix of numbers and strings): ").split()
+    user_data_input = input("Enter elements separated by spaces: ").split()
     number_array = []
     string_array = []
+    print("User Entered Array: ", user_data_input)
+    global originalArr
+    originalArr = utilities.dictionary(user_data_input)
     for item in user_data_input:
-
         if(item.isdigit()):
             number_array.append(item)
-        elif not (bool(re.search(r'[^A-Za-z0-9]', item))):
+        elif not (bool(re.search(r'[^A-Za-z]', item))):
             string_array.append(item)
         else:
             pass
@@ -57,6 +59,9 @@ def generateStringData():
 
     dataSize = int(input("Enter the size of string array to be generated: "))
     string_array = utilities.generate_StringArray(dataSize)
+    print("Generated Unsorted String Array: ", string_array)
+    global originalArr
+    originalArr = utilities.dictionary(string_array)
     return string_array
 
 def generateNumberData():
@@ -69,9 +74,13 @@ def generateNumberData():
 
     dataSize = int(input("Enter the size of number array to be generated: "))
     number_array = utilities.generate_numberArray(dataSize)
+    print("Generated Unsorted Number Array: ", number_array)
+    global originalArr
+    originalArr = utilities.dictionary(number_array)
     return number_array
 
-user_input = int(input("Enter 1 to enter input array or Enter 2 to auto-generate string input arrays or Enter 3 to auto-generate number input arrays: "))
+
+user_input = int(input("Enter 1 to enter input array\nEnter 2 to auto-generate string input arrays\nEnter 3 to auto-generate number input arrays\nEntered value: "))
 
 if user_input == 1:
     number_array, string_array = enterData()
@@ -95,8 +104,6 @@ utilities.createRecord(numbDict, ncr)
 strVal = list(strDict.values())
 numbVal = list(numbDict.values())
 
-index2Check = int(input("Enter index value of element in sorted array to check its index in original array: "))
-
 algorithms = {
         ms.merge_sort,
         rs.radix_sort,
@@ -105,24 +112,19 @@ algorithms = {
     }
 i = 0
 for algo in algorithms:
-    if len(string_array) != 0:
-        start_time = time.time()
-        sorted_array = algo(string_array)
-        end_time = time.time()
-        utilities.add_data(performanceData, i, algo.__name__, end_time-start_time)
-        index_str = strVal.index(sorted_array[index2Check])
-        print("Sorted arrays' element: ", sorted_array[index2Check], " at index: ", index2Check, " is at index: ", index_str, " in the original array")
-        i+=1
-    elif len(number_array) != 0:
-        start_time = time.time()
-        sorted_array = algo(number_array)
-        end_time = time.time()
-        utilities.add_data(performanceData, i, algo.__name__, end_time-start_time)
-        print(numbVal)
-        index_numb = numbVal.index(sorted_array[index2Check])
-        print("Sorted arrays' element: ", sorted_array[index2Check], " at index: ", index2Check, " is at index: ", index_numb, " in the original array")
-        i+=1
+    if (len(string_array) != 0) or (len(number_array) !=0):
+            start_time = time.time()
+            sorted_array_s = algo(string_array)
+            sorted_array_n = algo(number_array)
+            end_time = time.time()
+            utilities.add_data(performanceData, i, algo.__name__, end_time-start_time)
+            print("Sorted String Array: ", sorted_array_s)
+            print("Sorted Number Array: ", sorted_array_n)
+            i+=1
     else:
+        print("Invalid input entered/Selected index in dictionary is not correct")
         pass
+
+    print("Original array with index of constituent elements: ", originalArr)
 
 utilities.export2CSV(performanceData)
